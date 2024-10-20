5f32313ea5e388eff544fbff366e07d85d2d2464
Braden Godley <braden@bgodley.com>
2024-10-19 20:31:52 -0700

    improve mobile layout

--- resources/views/livewire/color.blade.php
+++ resources/views/livewire/color.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="w-80 h-80 mb-2" style="background-color: {{ $color->color }}; box-shadow: 5px 5px rgba(0,0,0,.1)">
+    <div class="lg:w-80 lg:h-80 md:w-64 md:h-64 w-32 h-32 mb-2" style="background-color: {{ $color->color }}; box-shadow: 5px 5px rgba(0,0,0,.1)">
     </div>
     <span>{{ $color->color }}</span>
 </div>
--- resources/views/livewire/game.blade.php
+++ resources/views/livewire/game.blade.php
@@ -1,7 +1,7 @@
 <div class="w-screen h-screen flex flex-col justify-center items-center">
-    <h1 class="text-5xl font-bold mb-16">The Best Color Ever</h1>
+    <h1 class="lg:text-5xl text-2xl font-bold mb-16">The Best Color Ever</h1>
     <p class="mb-20">Which color is better?</p>
-    <div class="flex gap-16">
+    <div class="flex lg:gap-16 gap-4">
         <button wire:click="vote(1)">
             <livewire:color :color="$color1" />
         </button>
